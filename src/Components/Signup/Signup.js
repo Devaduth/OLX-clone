aa0b6f79a3b793f6d3cc2dd9f22aeb8ee6f5cec6
Devaduth <kmdevaduth@gmail.com>
2022-07-08 19:49:26 +0530

    day 1 SignUp page

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,53 +1,111 @@
-import React from 'react';
-
+//imports
+import React, { useContext, useState } from 'react';
+import { useForm } from "react-hook-form"
 import Logo from '../../olx-logo.png';
+import { FirebaseContext } from '../../store/firebaseContext';
 import './Signup.css';
+import { useHistory } from 'react-router-dom'
+
+
+
+//imports 
 
 export default function Signup() {
+  const { register, formState: { errors } } = useForm();
+  const history = useHistory()
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
+  const { firebase } = useContext(FirebaseContext)
+ 
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // console.log(firebase);
+    firebase.auth().createUserWithEmailAndPassword(email, password).then((result) => {
+      result.user.updateProfile({ displayName: username }).then(() => {
+        firebase.firestore().collection('users').add({
+          id: result.user.uid,
+          username: username,
+          phone: phone
+        }).then(() => {
+          history.push("/login")
+        })
+      })
+    }).catch((error) => {
+      alert(error);
+   })
+
+  }
+
   return (
     <div>
       <div className="signupParentDiv">
         <img width="200px" height="200px" src={Logo}></img>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="fname">Username</label>
           <br />
           <input
             className="input"
             type="text"
+            {...register("username", { required: true, maxLength: 14 })}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
             id="fname"
             name="name"
             defaultValue="John"
           />
+          {errors.username && <p>Please check the username</p>}
           <br />
           <label htmlFor="fname">Email</label>
           <br />
           <input
             className="input"
             type="email"
+            {...register("email",
+              {
+                required: true,
+                pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+              })}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             id="fname"
             name="email"
             defaultValue="John"
           />
+          {errors.email && <p>Please check the Email</p>}
           <br />
           <label htmlFor="lname">Phone</label>
           <br />
           <input
             className="input"
             type="number"
+            value={phone}
+            {...register("phone", { required: true, maxLength: 10, minLength: 10 })}
+            onChange={(e) => setPhone(e.target.value)}
             id="lname"
             name="phone"
             defaultValue="Doe"
           />
+          {errors.phone && <p>Please check the phone</p>}
           <br />
           <label htmlFor="lname">Password</label>
           <br />
           <input
             className="input"
             type="password"
+            value={password}
+            {...register("password", {
+              required: true,
+              pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}$/
+            })}
+            onChange={(e) => setPassword(e.target.value)}
             id="lname"
             name="password"
             defaultValue="Doe"
           />
+          {errors.password && <p>Please check the Password</p>}
           <br />
           <br />
           <button>Signup</button>
---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+import 'firebase/auth'
+import 'firebase/firebase'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD3QOgYB2UR883S_Fv0CNjLQqja2ju4fU0",
+    authDomain: "olx-cloneapp-7395a.firebaseapp.com",
+    projectId: "olx-cloneapp-7395a",
+    storageBucket: "olx-cloneapp-7395a.appspot.com",
+    messagingSenderId: "186456769705",
+    appId: "1:186456769705:web:0d1121f892bb8d880795b8",
+    measurementId: "G-ZC26T55DNF"
+};
+export default firebase.initializeApp(firebaseConfig);
